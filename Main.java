96859a6761c6bbe8a1eccf139a2da7878aa553eb
harishbommakanti <33380236+harishbommakanti@users.noreply.github.com>
2019-06-22 13:00:40 -0500

    initial commit
    - skeleton code for the program

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main
+{
+    private static int numPastSemesters = 0;
+    private static double pastGPA = 0;
+    private static double totalGPA = 0;
+
+    public static void main(String[] args)
+    {
+        System.out.println("This program calculates your high school GPA based on Leander ISD's grade policies.");
+        System.out.println("This uses data from previous semesters and calculates the data for the new semester.");
+        enterPastData();
+        enterSemesterData();
+        enterFinalData();
+        System.out.println("Your total GPA is: " + totalGPA);
+    }
+
+    public static void enterPastData()
+    {
+    }
+
+    public static void enterSemesterData()
+    {
+    }
+
+    public static void enterFinalData()
+    {
+    }
+}
